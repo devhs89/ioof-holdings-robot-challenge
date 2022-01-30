cd907d373d2d77517d3e2bbadfb2a07fd179fe3f
Harpreet Singh <developer.hs89@gmail.com>
2022-01-31 02:09:00 +1100

    FEATURE: make maven project for testing

---
+++ src/test/java/TableControllerTest.java
@@ -0,0 +1,10 @@
+import controllers.TableController;
+
+public class TableControllerTest {
+  TableController tc = new TableController();
+
+  public void testCreateTable() {
+//    TODO - Start here
+  }
+
+}
